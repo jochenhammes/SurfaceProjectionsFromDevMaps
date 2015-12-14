b286fbd377d8ba7bb3f9c2f980aa89ac6895f4d6
jochenhammes <hammesj@iMac-MMNI.local>
2015-12-14 10:31:36 +0100

    Setup of repository
    Full working version from 12/14/2015

---
+++ BatchCreateSurfaceProjection.m
@@ -0,0 +1,36 @@
+%% Environment
+clear all;
+
+pathImagesToProcessFolder = '/Volumes/MMNI_RAID/RAID_MMNI/BatchScriptsMatlab/AutomatedProcessingStream/Images/NormalizedTest/DeviationMapsDecIncTau_new/';
+pathOutputFolder = [pathImagesToProcessFolder 'SP/'];
+
+%Create Output Directory
+mkdir(pathOutputFolder);
+
+%Find all Files with .nii-Ending in Input Folder
+subj=dir(strcat(pathImagesToProcessFolder,'DevInc*.nii'));
+numberOfFiles=length(subj);
+
+%% Run through all files in the input folder
+
+for FileNumber = 1:numberOfFiles
+    
+    %% Call function to create surface projections
+    
+    [OutputRGB, OutputNii, OutputMono] = CreateSurfProjFromNii([pathImagesToProcessFolder subj(FileNumber).name], 10, subj(FileNumber).name);
+    
+    
+    %% Save output
+        
+    
+    %Write Bitmaps
+    imwrite(OutputRGB, [pathOutputFolder 'SP_RGB'  subj(FileNumber).name '.bmp']);
+    imwrite(OutputMono, [pathOutputFolder 'SP_Mono'  subj(FileNumber).name '.bmp']);
+    
+    %write Nifti
+    save_nii(OutputNii, [pathOutputFolder 'SP_' subj(FileNumber).name] );
+    
+%     %ShowImage
+%     image(DisplayProjectionsRGB);
+    
+end
